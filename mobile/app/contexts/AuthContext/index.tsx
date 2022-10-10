db6fc789f58ff95f657aca0c2f58267a4347cab9
Renan Loureiro <renan.pereiral@sonda.com>
2022-10-10 09:04:19 -0300

    rehytraded authentication data

--- mobile/app/contexts/AuthContext/index.tsx
+++ mobile/app/contexts/AuthContext/index.tsx
@@ -1,3 +1,4 @@
+import { useAsyncStorage } from "../../hooks/useAsyncStorage";
 import React, {
   createContext,
   useState,
@@ -14,23 +15,44 @@ type AuthContextProviderType = {
 
 const AuthContext = createContext<AuthContextType>({} as AuthContextType);
 
+const USER_KEY = "naTrave:user";
+const CREDENTIALS_KEY = "naTrave:credentials";
+
 export const AuthContextProvider = ({ children }: AuthContextProviderType) => {
   const [user, setUser] = useState<User>(null);
   const [credentials, setCredentials] = useState<Credentials>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const updatedCredentials = useCallback((credentials: Credentials) => {
+  const { getData, setData } = useAsyncStorage();
+
+  const updatedCredentials = useCallback(async (credentials: Credentials) => {
+    await setData(CREDENTIALS_KEY, credentials);
     setCredentials(credentials);
   }, []);
 
-  const onSignIn = useCallback((user: User) => {
+  const onSignIn = useCallback(async (user: User) => {
+    await setData(USER_KEY, user);
     setUser(user);
   }, []);
 
   useEffect(() => {
-    console.log("USER", user);
+    const rehydrated = async () => {
+      try {
+        const userData = await getData(USER_KEY);
 
-    console.log("TOKENS", credentials);
-  }, [user, credentials]);
+        const credentialsData = await getData(CREDENTIALS_KEY);
+
+        setCredentials(credentialsData);
+        setUser(userData);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    rehydrated();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, updatedCredentials, onSignIn }}>
---
+++ mobile/app/hooks/useAsyncStorage.ts
@@ -0,0 +1,30 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const setData = async (key: string, value: any) => {
+  try {
+    const valueFormatted = JSON.stringify(value);
+
+    await AsyncStorage.setItem(key, valueFormatted);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getData = async (key: string) => {
+  try {
+    const data = await AsyncStorage.getItem(key);
+
+    const dataFormatted = JSON.parse(data);
+
+    return data ? dataFormatted : null;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const useAsyncStorage = () => {
+  return {
+    setData,
+    getData,
+  };
+};
